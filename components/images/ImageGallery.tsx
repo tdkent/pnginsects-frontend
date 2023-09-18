b3e67a9f0e278b255c7630722e4456771ae89d96
Tim Kent <timkentdev@gmail.com>
2023-09-18 14:58:22 -0700

    update logs

--- components/images/ImageGallery.tsx
+++ components/images/ImageGallery.tsx
@@ -9,5 +9,10 @@ interface Props {
 export default async function ImageGallery({ name }: Props) {
   const endpoint = name.toLowerCase()
   const { resources }: CloudinaryResources = await getImages(endpoint)
+  const numberOfResources = resources.reduce(
+    (acc, curr) => acc + curr.images.length,
+    0
+  )
+  console.log("numberOfResources ", numberOfResources)
   return <ImageGalleryContents resources={resources} />
 }

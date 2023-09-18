82329b7f3c967e4957ce6169a7b258f874a2179f
Tim Kent <timkentdev@gmail.com>
2023-09-18 16:34:03 -0700

    update revalidate

--- utils/funcs/index.tsx
+++ utils/funcs/index.tsx
@@ -4,7 +4,7 @@ import { backendUrl } from "../lib/constants"
 
 export const getImages = cache(async (endpoint: string) => {
   const response = await fetch(`${backendUrl}/${endpoint}`, {
-    next: { revalidate: 60 },
+    next: { revalidate: 1 },
   })
   return response.json()
 })

b50b3a2d884224f8da87f15b6129076a8844001c
Tim Kent <timkentdev@gmail.com>
2023-10-20 21:33:19 -0700

    add css hidden rule to back to top when on home page

--- components/layout/BackToTop.tsx
+++ components/layout/BackToTop.tsx
@@ -1,13 +1,15 @@
 "use client"
 import useStore from "@/utils/hooks"
+import { usePathname } from "next/navigation"
 import { ArrowUpCircleIcon } from "@heroicons/react/24/outline"
 
 export default function BackToTop() {
+  const pathname = usePathname()
   const isPageHeaderVisible = useStore((state) => state.isPageHeaderVisible)
   return (
     <a
       href="#"
-      className={`${
+      className={`${pathname === "/" && "hidden"} ${
         isPageHeaderVisible ? "opacity-0" : "opacity-100"
       } sticky bottom-6 left-full mb-6 mr-6 flex w-fit items-center justify-end gap-x-2 place-self-end whitespace-nowrap rounded-xl bg-black/60 p-4 text-white transition-opacity duration-300 dark:bg-neutral-500/75 max-lg:hidden`}
     >
